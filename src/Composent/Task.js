f53bd64d94dac271e2c01272aa5e4ebfbd0db77b
chaimae147 <chaymae902@gmail.com>
2023-01-24 23:28:45 +0100

    to do list version 2

--- src/Composent/Task.js
+++ src/Composent/Task.js
@@ -8,7 +8,7 @@ export default function Task({txt,delf,done,donecomplete}) {
     <div>
       
       <input type="text"  className='ajo'  style={done ? {backgroundColor:'rosybrown',border:'0.2 solid gray',color:'white',fontWeight:'bold'} : {backgroundColor:'white',border:'0.2 solid black'}} readOnly value={txt} /> &nbsp;&nbsp;
-    <br /> <input type="button"  onClick={delf} className='btn btn-outline-danger' value="SUPPRIMER" />&nbsp;&nbsp;
+     <input type="button"  onClick={delf} className='btn btn-outline-danger' value="SUPPRIMER" />&nbsp;&nbsp;
       <input type="button"  className='btn btn-outline-success' onClick={donecomplete} value="DONE" />
 <hr />
 
